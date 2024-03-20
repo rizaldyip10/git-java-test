8705d922a32b7e58467e5f58bd657f81a22739da
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-03-20 15:08:18 +0700

    gondangdia

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void test(String[] args) {
         System.out.println("Hello Jawa");
         System.out.println("Cikini");
+        System.out.println("Gondangdia");
     }
 }
